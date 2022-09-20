5830257266b8d17487bbca7356f37df093abc6ae
MarinauDev <marinawpdev@gmail.com>
2022-09-20 22:06:44 +0200

    Lint fix. Mobile text with error - mobile visible fix.

--- frontend/assets/js/helpers/error-message.js
+++ frontend/assets/js/helpers/error-message.js
@@ -1,45 +1,42 @@
 export const setErrorStatus = (field, message, status, form) => {
-    const errorMessage =  form.querySelector('.error-text');
-    const parentEl = field.parentElement;
+  const errorMessage = form.querySelector('.error-text')
+  const parentEl = field.parentElement
 
-    if(errorMessage) {
-      if (status === "success") {
-        field.classList.remove('input-error');
-        parentEl.classList.remove('error');
-        parentEl.classList.add('success');
-
-        errorText("",'success', form);
-      }
-
-      if (status === "error") {
-        parentEl.classList.remove('success');
-        parentEl.classList.add('error');
-
-        errorText(message,'error', form);
-      }
+  if(errorMessage) {
+    if (status === 'success') {
+      field.classList.remove('input-error')
+      parentEl.classList.remove('error')
+      parentEl.classList.add('success')
+      errorText('','success', form)
     }
-};
+
+    if (status === 'error') {
+      parentEl.classList.remove('success')
+      parentEl.classList.add('error')
+      errorText(message,'error', form)
+    }
+  }
+}
 
 export const errorText = (message,status, form) => {
-  const errorMessage =  form.querySelector('.error-text');
-
-  if (status === "success") {
+  const errorMessage = form.querySelector('.error-text')
+  if (status === 'success') {
     if (errorMessage) {
-      errorMessage.innerText = "";
-      errorMessage.classList.add('hidden');
-      errorMessage.classList.remove('success-send');
+      errorMessage.innerText = ''
+      errorMessage.classList.add('hidden')
+      errorMessage.classList.remove('success-send')
     }
   }
 
-  if (status === "error") {
-    errorMessage.innerText = message;
-    errorMessage.classList.remove('hidden');
-    errorMessage.classList.remove('success-send');
+  if (status === 'error') {
+    errorMessage.innerText = message
+    errorMessage.classList.remove('hidden')
+    errorMessage.classList.remove('success-send')
   }
 
-  if (status === "success-send") {
-      errorMessage.innerText = message;
-      errorMessage.classList.remove('hidden');
-      errorMessage.classList.add('success-send');
+  if (status === 'success-send') {
+    errorMessage.innerText = message
+    errorMessage.classList.remove('hidden')
+    errorMessage.classList.add('success-send')
   }
 }
